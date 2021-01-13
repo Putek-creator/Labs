a0d44e346cf04140f5d38e960af7a8bcd91e8340
Putek-creator <lukii20012@gmail.com>
2021-01-13 13:59:15 +0100

    lab12

--- lab 12/zad2.py
+++ lab 12/zad2.py
@@ -1,8 +1,7 @@
 import math
 r = int(input("Podaj promien: "))
-
 def kula(r):
-    p = r * 4 * math.pi
-    v = 4 / 3 * math.pi * math.pi
-    return v and p
+    p = math.pow(r,2) * 4 * math.pi
+    v = 4/3 * math.pi * math.pow(r,3)
+    print("Pole to ",p,", objętośc to", v)
 print(kula(r))
