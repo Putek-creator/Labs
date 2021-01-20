dde422f6933ef0c3b949a83cf43366c4d9be7a3a
Putek-creator <lukii20012@gmail.com>
2021-01-20 18:20:33 +0100

    lab12

---
+++ lab 12/zad3.py
@@ -0,0 +1,40 @@
+def cm(cale):
+    finish = f'W cm: {cale / 10}'
+    return finish
+
+
+def mm(cale):
+    finish = f'W mm: {cale * 10}'
+    return finish
+
+
+def m(cale):
+    finish = f'W m: {cale}'
+    return finish
+
+
+def km(cale):
+    finish = f'W km: {cale / 1000}'
+    return finish
+
+
+if __name__ == "__main__":
+    cal = int(input("Podaj cale: "))
+    cale = 2.54 * cal
+    print("Wpisz w co chcesz przekształcić cale: mm,cm,m,km")
+    while True:
+        what = input("Podaj co chcesz ode mnie: ")
+        if what == 'mm':
+            print(mm(cale))
+            break
+        if what == 'cm':
+            print(cm(cale))
+            break
+        if what == 'km':
+            print(km(cale))
+            break
+        if what == 'm':
+            print(m(cale))
+            break
+        else:
+            print('Invalid input. Podaj mm,cm,m,km')
